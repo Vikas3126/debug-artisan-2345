504e286e7682ee5897be2b9442dd6f34420888c3
AniketBaghel1601 <aniket357baghel@gmail.com>
2024-02-24 20:05:44 +0530

    Changes in server

--- backend/socket.js
+++ backend/socket.js
@@ -1,38 +1,38 @@
-// index.js (Server-side)
+// // index.js (Server-side)
 
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
+// const express = require('express');
+// const http = require('http');
+// const socketIO = require('socket.io');
+// const cors = require('cors');
 
-const PORT = 8080;
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const connectedUsers = {};
+// const PORT = 8080;
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIO(server);
+// const connectedUsers = {};
 
-// Allow requests from any origin
-app.use(cors());
+// // Allow requests from any origin
+// app.use(cors());
 
-app.use(express.static('index.html'));
+// app.use(express.static('index.html'));
 
-io.on("connection",(socket)=>{
-    socket.on('join-lobby',(userName)=>{
-        connectedUsers[socket.id] = {name : userName};
-        io.emit('lobby_info', Object.values(connectedUsers));
-    })
+// io.on("connection",(socket)=>{
+//     socket.on('join-lobby',(userName)=>{
+//         connectedUsers[socket.id] = {name : userName};
+//         io.emit('lobby_info', Object.values(connectedUsers));
+//     })
 
 
-    socket.on('disconnect', () => {
-        if (connectedUsers[socket.id]) {
-            const userName = connectedUsers[socket.id].name;
-            delete connectedUsers[socket.id];
-            console.log(`${userName} left the lobby`);
-            io.emit('lobby_info', Object.values(connectedUsers));
-        }
-    });
-});
+//     socket.on('disconnect', () => {
+//         if (connectedUsers[socket.id]) {
+//             const userName = connectedUsers[socket.id].name;
+//             delete connectedUsers[socket.id];
+//             console.log(`${userName} left the lobby`);
+//             io.emit('lobby_info', Object.values(connectedUsers));
+//         }
+//     });
+// });
 
-server.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//     console.log(`Server is running at port ${PORT}`);
+// });
