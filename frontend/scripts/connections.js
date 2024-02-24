45808856ddd324c4d58f261ce72b88152ed17ad8
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-02-24 19:20:56 +0530

    connection between html pages

---
+++ frontend/scripts/connections.js
@@ -0,0 +1,14 @@
+document.getElementById("Learnig").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "../learningTool.html"
+});
+
+document.getElementById("team").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "news.html"
+});
+
+document.getElementById("Home").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "../index.js"
+});
---
+++ frontend/scripts/logout.js
@@ -0,0 +1,42 @@
+
+function logout() {
+    const accessToken = localStorage.getItem("token");
+
+    fetch(`http://localhost:4400/users/logout`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                localStorage.removeItem("token")
+                return response.json();
+            } else {
+                throw new Error(`Logout failed: ${response.statusText}`);
+            }
+        })
+        .then((result) => {
+            console.log(result.msg);
+            location.href = '../index.html';
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+document.getElementById("Learnig").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "../learningTool.html"
+});
+
+document.getElementById("team").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "news.html"
+});
+
+document.getElementById("Home").addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = "../index.html"
+});
