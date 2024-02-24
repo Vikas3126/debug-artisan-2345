7f6167e65aaa9c3caf2921e2cb51461c5eac3bbb
AniketBaghel1601 <aniket357baghel@gmail.com>
2024-02-24 23:36:22 +0530

    popup added

--- backend/controller/routes/manualLogin.route.js
+++ backend/controller/routes/manualLogin.route.js
@@ -161,7 +161,7 @@ userRouter.get('/:user_id', async (req, res) => {
 
   try {
 
-    const user = await UserModel.findById(userId);
+    const user = await User.findById(userId);
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
