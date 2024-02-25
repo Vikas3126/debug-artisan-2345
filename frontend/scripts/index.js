1f6b158a95008009fe51f130157ac5333296b07c
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-02-25 22:42:55 +0530

    root folder prblem rectify

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,26 +1,26 @@
 // script file for landing page
 document.getElementById("Learnig").addEventListener("click", function (e) {
     e.preventDefault()
-    window.location.href = "./view/learningTool.html"
+    window.location.href = "./frontend/view/learningTool.html"
 });
 
 document.getElementById("team").addEventListener("click", function (e) {
     e.preventDefault()
-    window.location.href = "./view/news.html"
+    window.location.href = "./frontend/view/news.html"
 });
 
 document.getElementById("view-btn").addEventListener("click", function (e) {
     e.preventDefault()
-    window.location.href = "./view/news.html"
+    window.location.href = "./frontend/view/news.html"
 });
 
 document.getElementById("reg-btn").addEventListener("click", function (e) {
     e.preventDefault()
-    window.location.href = "./view/loginPage.html#"
+    window.location.href = "./frontend/view/loginPage.html#"
 });
 document.getElementById("log-btn").addEventListener("click", function (e) {
     e.preventDefault()
-    window.location.href = "./view/loginPage.html"
+    window.location.href = "./frontend/view/loginPage.html"
 });
 function toggleMenu() {
     var innerNavbar = document.querySelector('.inner-navbar');
--- frontend/scripts/logout.js
+++ frontend/scripts/logout.js
@@ -1,51 +1,50 @@
-
 function logout() {
-    const accessToken = localStorage.getItem("token");
+  const accessToken = localStorage.getItem("token");
 
-    fetch(`https://type-racing-speedster.onrender.com/users/logout`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`,
-        },
+  fetch(`https://type-racing-speedster.onrender.com/users/logout`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        localStorage.removeItem("token");
+        return response.json();
+      } else {
+        throw new Error(`Logout failed: ${response.statusText}`);
+      }
     })
-        .then((response) => {
-            if (response.ok) {
-                localStorage.removeItem("token")
-                return response.json();
-            } else {
-                throw new Error(`Logout failed: ${response.statusText}`);
-            }
-        })
-        .then((result) => {
-            console.log(result.msg);
-            location.href = '../index.html';
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    .then((result) => {
+      console.log(result.msg);
+      location.href = "../../index.html";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 document.getElementById("Learnig").addEventListener("click", function (e) {
-    e.preventDefault()
-    window.location.href = "../view/learningTool.html"
+  e.preventDefault();
+  window.location.href = "../view/learningTool.html";
 });
 
 document.getElementById("team").addEventListener("click", function (e) {
-    e.preventDefault()
-    window.location.href = "../view/news.html"
+  e.preventDefault();
+  window.location.href = "../view/news.html";
 });
 
 document.getElementById("Home").addEventListener("click", function (e) {
-    e.preventDefault()
-    window.location.href = "../public/dashboard.html"
+  e.preventDefault();
+  window.location.href = "../public/dashboard.html";
 });
 
 document.getElementById("achievements").addEventListener("click", function (e) {
-    e.preventDefault()
-    window.location.href = "../view/achivement.html"
+  e.preventDefault();
+  window.location.href = "../view/achivement.html";
 });
 function toggleMenu() {
-    var innerNavbar = document.querySelector('.inner-navbar');
-    innerNavbar.classList.toggle('show');
-  }
+  var innerNavbar = document.querySelector(".inner-navbar");
+  innerNavbar.classList.toggle("show");
+}
