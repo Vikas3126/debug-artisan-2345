4caf00691620d55f0e2f88f1579de2283eefe86b
AniketBaghel1601 <aniket357baghel@gmail.com>
2024-02-25 18:25:48 +0530

    tracks added

--- frontend/scripts/multiplayer.js
+++ frontend/scripts/multiplayer.js
@@ -1,9 +1,6 @@
-window.onload = function() {
+const userInterface = document.getElementById('multiplayer-interface');
     const username = sessionStorage.getItem("username");
-
-    if (username) {
-        const socket = io("http://localhost:4400/", { transports: ['websocket'] });
-
+    const socket = io("http://localhost:4400/", { transports: ['websocket'] });
         socket.on("connect", () => {
             socket.emit("join-lobby", username);
         });
@@ -11,5 +8,24 @@ window.onload = function() {
         //     socket.emit("loby-message", username,message);
         // });
         // Additional socket event listeners can be added here
-    }
-};
+    
+
+
+
+
+        socket.on("connections_count", (count) => {
+            // Clear existing tracks
+            userInterface.innerHTML = '';
+        
+            // Create tracks for each connection
+            for (let i = 0; i < count && i < 4; i++) {
+                let usersTrack = document.createElement('div');
+                usersTrack.className = "user-track";
+                let track = document.createElement('h2');
+                track.textContent = "- - - - - - - - - - - - - - - - - - - - - - - - - -";
+                track.className = "track";
+                usersTrack.append(track);
+                userInterface.append(usersTrack);
+            }
+        });
+
